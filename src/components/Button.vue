359e0ad9877c03c9a414afa11a37f7c981ec668d
ssonggary <84754325+ssonggary@users.noreply.github.com>
2024-08-08 15:23:10 +0900

    버튼컴포넌트 만들기 연습

---
+++ src/components/Button.vue
@@ -0,0 +1,61 @@
+<template>
+  <button 
+  type="button"
+  :class="['btn', variantClass]"
+  :disabled="disabled"
+  @click="handleClick"
+  >
+    <i 
+    v-if="icon"
+    :class="iconClass"
+    class="btn__icon"
+    ></i>
+    <span class="btn__text"><slot></slot></span>
+  </button>
+</template>
+<script>
+import { computed} from 'vue';
+
+export default {
+  name: 'Button',
+  props: {
+    variant: {
+      type: String,
+      default: 'default',
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+  },
+
+  setup(props) {
+    const {variant, disabled, icon} = props;
+
+    const handleClick = (e) => {
+      console.log('asdasdasd');
+    };
+
+    const variantClass = computed(() => {
+      return `btn--${variant}`
+    })
+    const iconClass = computed(() => {
+      return `icon-${icon}`
+    })
+
+    return {
+      variantClass,
+      handleClick,
+      iconClass
+    }
+  },
+}
+</script>
+<style scoped>
+  
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
     Home page
+    <Button>접수</Button>
+    <Button disabled>취소</Button>
+    <Button variant="line">신규</Button>
+    <Button icon="delete" variant="line">삭제</Button>
+    <Button icon="link">CHECK</Button>
   </div>
 </template>
 <script>
+import Button from '@/components/Button.vue';
+
 export default {
-  
+  components: {
+    Button,
+  },
 }
 </script>
 <style >
